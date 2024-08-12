15a71dd378b39ab7476c6b1dac0a986308e965fd
zenitsuleech <zenistuleech@gmail.com>
2024-08-12 11:41:40 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -26,9 +26,7 @@ async def allowed(_, __, message):
         return True
     if message.from_user and message.from_user.id in ADMINS:
         return True
-    if message.form_user and message.form_user.id not in ADMINS:
-        USER_TXT = "</b> You Are Not Admin"
-        return False
+    return False
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
